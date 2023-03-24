aacab83a0872a040f1bbee5e2e8525cc101845b1
lorenacmora <lcma1981@gmail.com>
2023-03-24 20:26:35 +0000

    prueba

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planets, Favorites
 #from models import Person
 
 app = Flask(__name__)
@@ -37,13 +37,112 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def handle_user():
+    if request.method == "GET":
+        users = User.query.all()
+        users_list =[]
+        for user in users:
+            users_list.append(user.serialize())
+            
+            return jsonify(users_list), 200
+        
+@app.route('/user/favorites', methods=['GET'])
+def handleuser_favorites():
+    if request.method == 'GET':
+        search_favoritebyuser = Favorites.query.all()
+        # favoritebyuser =[]
+        # for favorite in search_favoritebyuser:
+        #     favoritebyuser.append(favorite.serialize())
+        return (list(map(lambda favorite:favorite.serialize(),search_favoritebyuser)))
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/people', methods=["GET"])
+def getpeople():
+    if request.method =="GET":
+        people = People.query.all()
+        person_list= []  
+        for person in people:
+            person_list.append(person.serialize())
+        
+        return jsonify(person_list),200
 
-    return jsonify(response_body), 200
+@app.route('/planets', methods=["GET"])
+def getplanets():
+    if request.method == "GET":
+        planets = Planets.query.all()
+        planets_list =[]
+        for planet in planets:
+            planets_list.append(planet.serialize())
+
+        return jsonify(planets_list), 200
+
+@app.route('/people/<int:people_id>', methods=["GET"])
+def getpeople_id(people_id=None):
+    if request.method == "GET":
+        person_id = People.query.filter_by(id=people_id).first()
+        if person_id is None:
+            return jsonify({"Message":"There's no person with that ID"}),404
+        else:
+            return jsonify(person_id.serialize()),200
+        
+@app.route('/planets/<int:planets_id>', methods=["GET"])
+def planets_id(planets_id=None):
+    if request.method == "GET":
+        planets_id = Planets.query.filter_by(id=planets_id).first()
+        if planets_id is None:
+            return jsonify({"Message":"There's no person with that ID"}),404
+        else:
+            return jsonify(planets_id.serialize()),200
+
+@app.route('/favorites/people/<int:nature_id>',methods=['POST'])
+def getfavorites_people(nature_id=None):
+    if People.query.filter_by(id=nature_id).first():
+        search_favorite = Favorites.query.filter_by(user_id=1, nature_id=nature_id).first()
+        if search_favorite:
+            return jsonify({"Message":"Favorite already exist"}),400
+        else:
+            if request.method == 'POST':
+                favorite = Favorites(user_id=1, nature='people', nature_id=nature_id)
+                db.session.add(favorite)
+                db.session.commit()
+            return jsonify({"Message":"Favorite has been added to the user"}),201
+    else:
+        return jsonify({"Message":"People don't exist"}),400    
+
+@app.route('/favorites/planets/<int:nature_id>', methods=['POST'])
+def getfavorites_planets(nature_id=None):
+    if Planets.query.filter_by(id=nature_id).first():
+        search_favorite= Favorites.query.filter_by(user_id=1, nature_id=nature_id).first()
+        if search_favorite:
+            return jsonify({'Message':'Favorite already exist'}), 400
+        else:
+            if request.method == 'POST':
+                favorite= Favorites(user_id=1, nature='planets', nature_id=nature_id)
+                db.session.add(favorite)
+                db.session.commit()
+            return jsonify({'Message':'Favorite has been added to user'}),201
+    else:
+        return jsonify({'Message':'Planet doesnt exist'}),400
+
+@app.route('/favorites/planets/<int:nature_id>', methods=['DELETE'])
+def delete_planets(nature_id=None):
+    search_favorite= Favorites.query.filter_by(user_id=1, nature='planets', nature_id=nature_id).first()
+    if search_favorite:
+        db.session.delete(search_favorite)
+        db.session.commit()
+        return jsonify({'Message':'Favorite has been deleted successfully'}),201
+    else:
+        return jsonify({'Message':'Favorite that want to be deleted doesnt exist'}),400
+
+@app.route('/favorites/people/<int:nature_id>', methods=['DELETE'])
+def delete_people(nature_id=None):
+    search_favorite = Favorites.query.filter_by(user_id=1, nature="people", nature_id=nature_id).first()
+    if search_favorite:
+        db.session.delete(search_favorite)
+        db.session.commit()
+        return jsonify({'Message':'Favorite has been deleted successfully'})
+    else:
+        return jsonify({'Message':'Favorite that would like to be deleted doesnt exist'})
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Enum, ForeignKey
+import enum
 
 db = SQLAlchemy()
 
@@ -7,13 +9,83 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship("Favorites", backref="user", lazy=True)
+    
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+      
+            # do not serialize the password, its a security breach
+        }
+
+class People(db.Model):
+    __tablename__ = "people"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120),  nullable=False)
+    height = db.Column(db.String(120),  nullable=False)
+    eye_color = db.Column(db.String(120),  nullable=False)
+    hair_color = db.Column(db.String(120),  nullable=False)
+
+    def __repr__(self):
+        return f'<People ${self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "eye_color": self.eye_color,
+            "hair_color": self.hair_color
+            # do not serialize the password, its a security breach
+        }
+
+    
+
+class Planets(db.Model):
+    __tablename__ = "planets"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False )
+    gravity = db.Column(db.String(120), nullable=False )
+    population = db.Column(db.String(120), nullable=False)
+    climate = db.Column(db.String(120), nullable=False)
+
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gravity": self.gravity,
+            "population": self.population,
+            "climate": self.climate
+            # do not serialize the password, its a security breach
+        }
+
+
+class Nature(enum.Enum):
+    planets = "planets"
+    people = "people"
+
+class Favorites(db.Model):
+    __tablename__ = "favorites"
+    id = db.Column(db.Integer, primary_key=True)
+    nature_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, ForeignKey("user.id"), nullable=False)
+    nature = db.Column("nature",Enum(Nature))
+
+    def __repr__(self):
+        return f'<Favorites {self.nature} {self.nature_id}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nature_id":self.nature_id,
+            "user_id": self.user_id,
+            "nature": self.nature.value
+
             # do not serialize the password, its a security breach
         }
